944535dae80327e066c125896293c34915581152
AliMusa04 <tu75nx176@code.edu.az>
2022-12-13 01:47:40 -0800

    first commit

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Weapon weapon = new Weapon(50, 25, 2, "automatic");
+            
+
+            repeat:
+            Console.WriteLine("Reqem daxil edin:");
+            int num = int.Parse(Console.ReadLine());
+            switch (num)
+            {
+                case 0 : 
+                    Console.WriteLine("0 - İnformasiya almaq üçün\r\n1 - Shoot metodu üçün\r\n2 - Fire metodu üçün\r\n3 - GetRemainBulletCount metodu üçün\r\n4 - Reload metodu üçün\r\n5 - ChangeFireMode metodu üçün\r\n6 - Proqramdan dayandırmaq üçün");
+                    goto repeat;
+                    break;
+                case 1 : 
+                    Console.WriteLine("Shoot metodu ucun");
+                    weapon.Shoot();
+                    goto repeat;
+                    break;
+                case 2: 
+                    Console.WriteLine("Fire metodu üçün");
+                    weapon.Fire();
+                    goto repeat;
+                    break;
+                case 3: 
+                    Console.WriteLine("GetRemainBulletCount metodu üçün"); 
+                    weapon.GetRemainBulletCount();
+                    goto repeat;
+                    break;
+                case 4: 
+                    Console.WriteLine("Reload metodu üçün"); 
+                    weapon.Reload();
+                    goto repeat;
+                    break;
+                case 5: 
+                    Console.WriteLine("ChangeFireMode metodu üçün");
+                    weapon.ChangeFireMode();
+                    goto repeat;
+                    break;
+                case 6: 
+                    Console.WriteLine(" Proqramdan dayandırmaq üçün");
+                    goto repeat;
+                    break;
+                default:
+                    Console.WriteLine("Duzgun reqem daxil edin");
+                    goto repeat;
+                    break;
+            }
+        }
+    }
+}
---
+++ ConsoleApp1/Weapon.cs
@@ -0,0 +1,72 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+    internal class Weapon
+    {
+		private byte _bulletCapacity; /*daragin gulle tutumu*/
+        private byte _numberBullets; /*daragdaki gulle sayi*/
+        private byte _chargeSecond; /*daragin bosalma saniyesi*/
+        private string _automatic; /*atis modu (avtomatik)*/
+
+        public byte BulletCapacity
+		{
+			get { return _bulletCapacity; }
+			set { _bulletCapacity = value; }
+		}
+		public byte NumberBullets
+		{
+			get { return _numberBullets; }
+			set { _numberBullets = value; }
+		}
+		public byte ChargeSecond
+		{
+			get { return _chargeSecond; }
+			set { _chargeSecond = value; }
+		}
+		public string Automatic
+		{
+			get { return _automatic; }
+			set { _automatic = value; }
+		}
+
+		public Weapon(byte bulletCapacity, byte numberBullets, byte chargeSecond, string automatic)
+		{
+			BulletCapacity = bulletCapacity;
+			NumberBullets = numberBullets;
+			ChargeSecond = chargeSecond;
+			Automatic = automatic;
+		}
+        byte numberBullets = 70;
+        byte chargeSecond = 2;
+        byte numberBullet = 20;
+		private int bulletCapacity;
+
+        public void Shoot()
+		{
+			Console.WriteLine("ates acildi.");
+		}
+		public void Fire()
+		{
+			Console.WriteLine($"daraqdaki gullenin nece saniyede bitdiyini gosterir: {numberBullets / chargeSecond}");
+		}
+		public void GetRemainBulletCount()
+		{
+			Console.WriteLine($"daragin tam dolmasi ucun lazim olan gullenin sayi: {bulletCapacity - numberBullets}");
+		}
+		public void Reload()
+		{
+
+			Console.WriteLine($"daragi yeniden doldurur: {bulletCapacity - numberBullets}");
+		}
+		public void ChangeFireMode()
+		{
+
+		}
+
+    }
+}
